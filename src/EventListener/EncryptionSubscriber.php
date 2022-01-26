17b3a04a8381271ee5c44c24a8995a61f962ef5c
JBlairy <jblairy@insitaction.com>
2022-01-26 17:49:03 +0100

    fix: cache problems

--- src/EventListener/EncryptionSubscriber.php
+++ src/EventListener/EncryptionSubscriber.php
@@ -9,14 +9,14 @@ use Exception;
 use Insitaction\FieldEncryptBundle\Annotations\Encrypt;
 use Insitaction\FieldEncryptBundle\Model\EncryptedFieldsInterface;
 use Insitaction\FieldEncryptBundle\Service\EncryptService;
-use Insitaction\FieldEncryptBundle\Service\Misc\CacheItem;
 use ReflectionClass;
 
 class EncryptionSubscriber implements EventSubscriber
 {
+    public const ENCRYPTION_MARKER = '<ENC>';
+
     public function __construct(
-        private EncryptService $encryptService,
-        private CacheItem $cacheItem,
+        private EncryptService $encryptService
     ) {
     }
 
@@ -55,35 +55,17 @@ class EncryptionSubscriber implements EventSubscriber
         }
     }
 
-    public function postUpdate(LifecycleEventArgs $args): void
-    {
-        $object = $args->getObject();
-        if ($object instanceof EncryptedFieldsInterface) {
-            $this->decryptFields($object);
-        }
-    }
-
-    public function postLoad(LifecycleEventArgs $args): void
-    {
-        $object = $args->getObject();
-        if ($object instanceof EncryptedFieldsInterface) {
-            $this->decryptFields($object);
-        }
-    }
-
     private function encryptFields(EncryptedFieldsInterface $entity): void
     {
-        /** @var class-string $entityClass */
-        $entityClass = str_replace('Proxies\__CG__\\', '', get_class($entity));
-
         foreach ((new ReflectionClass($entity))->getProperties() as $reflectionproperty) {
             if (0 !== count($reflectionproperty->getAttributes(Encrypt::class))) {
                 $set = 'set' . ucfirst($reflectionproperty->name);
                 $get = 'get' . ucfirst($reflectionproperty->name);
 
                 if (is_callable([$entity, $set]) && is_callable([$entity, $get])) {
-                    $entity->$set($this->encryptService->encrypt($entity->$get()));
-                    $this->cacheItem->remove(md5($entityClass . 'decrypt' . $entity->getUniqueIdentifier() . $get));
+                    if (self::ENCRYPTION_MARKER !== substr($entity->$get(), -strlen(self::ENCRYPTION_MARKER))) {
+                        $entity->$set($this->encryptService->encrypt($entity->$get() . self::ENCRYPTION_MARKER));
+                    }
                 } else {
                     throw new Exception('You need to define get' . ucfirst($reflectionproperty->name) . 'in entity ' . $entity::class);
                 }
@@ -101,26 +83,20 @@ class EncryptionSubscriber implements EventSubscriber
                 $get = 'get' . ucfirst($reflectionproperty->name);
 
                 if (is_callable([$entity, $set]) && is_callable([$entity, $get])) {
-                    $key = md5($entityClass . 'decrypt' . $entity->getUniqueIdentifier() . $get);
-                    $cache = $this->cacheItem->get($key);
-                    if (null === $cache) {
-                        if (!is_resource($entity->$get())) {
-                            $stream = fopen('php://memory', 'r+');
+                    if (!is_resource($entity->$get())) {
+                        $stream = fopen('php://memory', 'r+');
 
-                            if (false === $stream) {
-                                throw new Exception('Cant fopen.');
-                            }
-
-                            fwrite($stream, $entity->$get());
-                            rewind($stream);
-                            $entity->$set($stream);
+                        if (false === $stream) {
+                            throw new Exception('Cant fopen.');
                         }
 
-                        $decrypt = $this->encryptService->decrypt($entity->$get());
-                        $entity->$set($decrypt);
-                        $this->cacheItem->cache($decrypt, $key);
-                    } else {
-                        $entity->$set($cache);
+                        fwrite($stream, $entity->$get());
+                        rewind($stream);
+                        $entity->$set($stream);
+                    }
+                    $decrypt = $this->encryptService->decrypt($entity->$get());
+                    if (self::ENCRYPTION_MARKER === substr($decrypt, -strlen(self::ENCRYPTION_MARKER))) {
+                        $entity->$set(substr($decrypt, 0, -strlen(self::ENCRYPTION_MARKER)));
                     }
                 } else {
                     throw new Exception('You need to define get' . ucfirst($reflectionproperty->name) . 'in entity ' . $entity::class);
