e05735e1ed013e5d1b6d4b3106eddf79d70c9a46
JBlairy <jblairy@insitaction.com>
2023-12-04 11:19:04 +0100

    feat: now accept sf7

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('field_encrypt');
         $treeBuilder->getRootNode()
